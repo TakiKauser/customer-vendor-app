d2f7889b287e7bfbe1844b72e73d79737fe39238
TakiKauser <takikauserbadhon@gmail.com>
2022-03-13 03:08:39 +0600

    bug--fixed

--- src/components/Pages/Purchase/Purchase.js
+++ src/components/Pages/Purchase/Purchase.js
@@ -12,9 +12,32 @@ const Purchase = () => {
     const [vendors, setVendors] = useState([]);
     const [selectedVendor, setSelectedVendor] = useState();
 
+    const [submitDataFormat, setSubmitDataFormat] = useState({
+        result: [
+            {
+                vendor: [
+                    {
+                        vendor: "mahin",
+                        email: "xsdfsf@google.com",
+                        address: "habijabi",
+                        order_deadline: "2022-03-10",
+                        mobile: "1236486"
+                    }
+                ],
+                products: []
+            }
+        ]
+    });
+
     const [products, setProducts] = useState([]);
+    // const [productDetails, setProductDetails] = useState();
     const [selectedProducts, setSelectedProducts] = useState();
-    const [productDetails, setProductDetails] = useState();
+
+    const [selectedProductsData, setSelectedProductsData] = useState({});
+
+    useEffect(() => {
+        findSelectProductData(selectedProducts)
+    }, [selectedProducts]);
 
     useEffect(() => {
         fetch(`https://vatdj.herokuapp.com/parties/list/vendors/`)
@@ -29,21 +52,26 @@ const Purchase = () => {
         fetch(`https://vatdj.herokuapp.com/product/product_list/`)
             .then(response => response.json())
             .then(jsonData => {
-                // console.log(jsonData);
+                // console.log("all product", jsonData);
                 setProducts(jsonData);
             })
     }, []);
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //       setValue("country", "India");
-    //     }, 2000);
-    //   }, [setValue]);
+    useEffect(() => {
+        setSelectedVendor(vendors[0]);
+    }, [vendors]);
+
+    useEffect(() => {
+        setSelectedProducts(products[0]?.id);
+    }, [products]);
 
     const handleOnChangeVendor = (e) => {
         setSelectedVendor(e.target.value);
 
         let currentVendor = vendors?.find(vendor => vendor.id == e.target.value);
+        // console.log("currentVendor", currentVendor);
+        setSelectedVendor(currentVendor);
+        // setProductDetails(currentVendor);
 
         const mobile = document.getElementById("mobile");
         mobile.value = currentVendor.mobile;
@@ -56,85 +84,47 @@ const Purchase = () => {
 
         // console.log(currentVendor);
     }
-    const handleOnChangeProduct = (e) => {
-        setSelectedProducts(e.target.value);
-        // console.log(selectedProducts.id);
 
-        const url = `https://vatdj.herokuapp.com/purchase/product_details/${e.target.value}/`;
+    const addTableRow = () => {
+        const newData = { ...submitDataFormat };
+        newData.result[0].products.unshift(selectedProductsData);
+        console.log("spd",selectedProductsData);
+        setSubmitDataFormat(newData);
+        console.log("sdf",submitDataFormat);
+    }
+    
+    const findSelectProductData = (id) => {
+        const url = `https://vatdj.herokuapp.com/purchase/product_details/${id}/`;
         fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ id: e.target.value })
+            body: JSON.stringify({ id: id })
         })
             .then(response => response.json())
             .then(result => {
-                console.log(result);
-                setProductDetails(result);
+                console.log("result", result);
+                setSelectedProductsData(result);
                 if (result.insertedId) {
                     alert("Got product successfully!");
                     reset();
                 }
             });
-        console.log("Det", productDetails);
-        // let currentProduct = products?.find(vendor => vendor.id == e.target.value);
-
-        const variant = document.getElementById("col1");
-        variant.innerHTML = productDetails?.product_variant;
-
-        const hs_code = document.getElementById("col2");
-        hs_code.innerHTML = productDetails?.hs_code;
-
-        const uom = document.getElementById("col3");
-        uom.innerHTML = productDetails?.uom;
-
-        const cd = document.getElementById("col4");
-        cd.innerHTML = productDetails?.cd;
-
-        const sd = document.getElementById("col5");
-        sd.innerHTML = productDetails?.sd;
-
-        const vat = document.getElementById("col6");
-        vat.innerHTML = productDetails?.vat;
-
-        const ait = document.getElementById("col7");
-        ait.innerHTML = productDetails?.ait;
-
-        const rd = document.getElementById("col8");
-        rd.innerHTML = productDetails?.rd;
-
-        const atv = document.getElementById("col9");
-        atv.innerHTML = productDetails?.atv;
-
-    }
-
-    const addTableRow = () => {
-        const table = document.getElementById("emptyTable");
-        const rowCount = table.rows.length;
-        const cellCount = table.rows[0].cells.length;
-        const row = table.insertRow(rowCount);
-
-        for (let i = 0; i < cellCount; i++) {
-            let cell = 'cell' + i;
-            cell = row.insertCell(i);
-            const copyCell = document.getElementById('col' + i).innerHTML;
-            cell.innerHTML = copyCell;
-            // if (i == 0) {
-
-            // }
-        }
     }
 
     const onSubmit = data => {
-        // data.vendor = selectedVendor;
-        const url = ``;
+        data.vendor = selectedVendor.name;
+
+        submitDataFormat.result[0].vendor = data;
+
+        const url = `https://vatdj.herokuapp.com/purchase/`;
         fetch(url, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(submitDataFormat)
         })
             .then(response => response.json())
             .then(result => {
@@ -144,7 +134,7 @@ const Purchase = () => {
                     reset();
                 }
             });
-        // console.log(data);
+        console.log("data", submitDataFormat);
     };
     return (
         <div>
@@ -152,17 +142,18 @@ const Purchase = () => {
             <h2 className="my-3">Purchase Invoice</h2>
             <h4 className="text-start ms-5">Vendor Info</h4>
             <form className="purchase-form" onSubmit={handleSubmit(onSubmit)}>
+                <input type="submit" value="Purchase" className="" />
                 <div className="d-flex justify-content-around">
                     <select onChange={(e) => handleOnChangeVendor(e)} value={selectedVendor?.id} name="vendor" id="vendor">
                         {/* <option value="">vendor</option> */}
                         {
                             vendors?.map((vendor, index) => (
                                 // console.log("vendor", vendor.description, index);
-                                <option value={`${vendor?.id}`} key={index}>{`${vendor?.name}`} </option>
+                                <option value={vendor?.id} key={index}>{`${vendor?.name}`} </option>
                             ))
                         }
                     </select>
-                    <input id="mobile" type="tel" placeholder="Mobile" value={selectedVendor?.mobile} {...register("mobile", { required: true, maxLength: 12 })} />
+                    <input id="mobile" type="tel" placeholder="Mobile" {...register("mobile", { required: true, maxLength: 12 })} />
                 </div>
                 <div className="d-flex">
                     <input id="email" type="text" placeholder="Email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
@@ -177,7 +168,6 @@ const Purchase = () => {
                 <Table id="emptyTable" striped bordered hover className="">
                     <thead>
                         <tr>
-                            {/* <th>#</th> */}
                             <th>Name</th>
                             <th>Variant</th>
                             <th>HS Code</th>
@@ -191,26 +181,40 @@ const Purchase = () => {
                         </tr>
                     </thead>
                     <tbody>
+                        {
+                            submitDataFormat.result[0].products.map((product, index) => (
+                                <tr key={index}>
+                                    <td id="col0">{product.product_name}</td>
+                                    <td id="col1">{product.product_variant}</td>
+                                    <td id="col2">{product.hs_code}</td>
+                                    <td id="col3">{product.uom}</td>
+                                    <td id="col4">{product.cd}</td>
+                                    <td id="col5">{product.sd}</td>
+                                    <td id="col6">{product.vat}</td>
+                                    <td id="col7">{product.ait}</td>
+                                    <td id="col8">{product.rd}</td>
+                                    <td id="col9">{product.atv}</td>
+                                </tr>
+                                // console.log(product)
+                            ))
+                        }
                         <tr>
-                            {/* <td>1</td> */}
                             <td id="col0">
-                                <select onChange={(e) => handleOnChangeProduct(e)} value={selectedProducts?.id} name="product" id="product">
+                                <select onChange={(e) => setSelectedProducts(e.target.value)} value={selectedProducts?.id} name="product" id="product">
                                     {
-                                        products?.map((product, index) => (
-                                            <option value={`${product?.id}`} key={index}>{`${product?.name}`} </option>
-                                        ))
+                                        products?.map((product, index) => <option value={product?.id} key={index}>{product?.name}</option>)
                                     }
                                 </select>
                             </td>
-                            <td id="col1"></td>
-                            <td id="col2"></td>
-                            <td id="col3"></td>
-                            <td id="col4"></td>
-                            <td id="col5"></td>
-                            <td id="col6"></td>
-                            <td id="col7"></td>
-                            <td id="col8"></td>
-                            <td id="col9"></td>
+                            <td id="col1">{selectedProductsData.product_variant}</td>
+                            <td id="col2">{selectedProductsData.hs_code}</td>
+                            <td id="col3">{selectedProductsData.uom}</td>
+                            <td id="col4">{selectedProductsData.cd}</td>
+                            <td id="col5">{selectedProductsData.sd}</td>
+                            <td id="col6">{selectedProductsData.vat}</td>
+                            <td id="col7">{selectedProductsData.ait}</td>
+                            <td id="col8">{selectedProductsData.rd}</td>
+                            <td id="col9">{selectedProductsData.atv}</td>
                         </tr>
                     </tbody>
                 </Table>
@@ -220,22 +224,8 @@ const Purchase = () => {
                     </Button>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
 export default Purchase;
-
-
-{/* <tr> */ }
-{/* <td>Jacob</td> */ }
-{/* <td>Thornton</td> */ }
-{/* <td>@fat</td> */ }
-{/* <td>Jacob</td> */ }
-{/* <td>Thornton</td> */ }
-{/* <td>@fat</td> */ }
-{/* <td>Jacob</td> */ }
-{/* <td>Thornton</td> */ }
-{/* <td>@fat</td> */ }
-{/* <td>@fat</td> */ }
-// </tr>
