50c392c2fc9247e6148c5b36c8e9c4e897326da6
TakiKauser <takikauserbadhon@gmail.com>
2022-03-14 03:29:29 +0600

    product-list

--- src/components/Pages/ProductDetails/ProductDetails.js
+++ src/components/Pages/ProductDetails/ProductDetails.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import Menubar from '../../Shared/Menubar/Menubar';
 
 const ProductDetails = () => {
+    const { itemNumber } = useParams();
+    const [list, setList] = useState([]);
+    const [productDetailsItem, setProductDetailsItem] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://vatdj.herokuapp.com/purchase/`)
+            .then(response => response.json())
+            .then(jsonData => {
+                // console.log("list", jsonData.result);
+                // console.log("item", jsonData.result[itemNumber].products[0].Biscuit);
+                setList(jsonData.result);
+                setProductDetailsItem(jsonData?.result[itemNumber]);
+                console.log(productDetailsItem);
+            })
+    }, [itemNumber, productDetailsItem]);
+
+    // useEffect(() => {
+    //     // const productDetailsItem = list.find(item => item.index == itemNumber);
+    //     console.log(productDetailsItem);
+    //     // setItem(careDetailsItem);
+    // }, []);
+
+    // console.log(itemNumber);
+
     return (
         <div>
             <Menubar />
+            <h4>ait: {productDetailsItem[0]?.products[0]?.Biscuit?.ait}</h4>
+            <h4>atv: {productDetailsItem?.products[0]?.Biscuit?.atv}</h4>
+            <h4>cd: {productDetailsItem?.products[0]?.Biscuit?.cd}</h4>
+            {/* <h4>product_id: {productDetailsItem?.products[0]?.Biscuit?.product_id}</h4>
+            <h4>product_type: {productDetailsItem?.products[0]?.Biscuit?.product_type}</h4>
+            <h4>product_variant: {productDetailsItem?.products[0]?.Biscuit?.product_variant}</h4>
+            <h4>rd: {productDetailsItem?.products[0]?.Biscuit?.rd}</h4>
+            <h4>remark: {productDetailsItem?.products[0]?.Biscuit?.remark}</h4>
+            <h4>sd: {productDetailsItem?.products[0]?.Biscuit?.sd}</h4>
+            <h4>total: {productDetailsItem?.products[0]?.Biscuit?.total}</h4>
+            <h4>uom: {productDetailsItem?.products[0]?.Biscuit?.uom}</h4>
+            <h4>vat: {productDetailsItem?.products[0]?.Biscuit?.vat}</h4> */}
         </div>
     );
 };
--- src/components/Pages/Products/Product-List/ProductList.js
+++ src/components/Pages/Products/Product-List/ProductList.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 import Menubar from '../../../Shared/Menubar/Menubar';
 
 const ProductList = () => {
+    const [productList, setProductList] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://vatdj.herokuapp.com/product/product_list/`)
+            .then(response => response.json())
+            .then(jsonData => {
+                // console.log("list", jsonData);
+                setProductList(jsonData);
+            })
+    }, []);
     return (
         <div>
-            <Menubar/>
-            <h3>List</h3>
+            <Menubar />
+            <div>
+                <h4 className="text-start ms-5 my-4">Product List</h4>
+                <Table id="table" striped bordered hover className="sm">
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Description</th>
+                            <th>Category</th>
+                            <th>Product Type</th>
+                            <th>Manufaturer</th>
+                            <th>Available in po</th>
+                            <th>Available in so</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            productList?.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item?.name}</td>
+                                    <td>{item?.description}</td>
+                                    <td>{item?.product_category}</td>
+                                    <td>{item?.product_type}</td>
+                                    <td>{item?.manufacturer}</td>
+                                    <td>{(item?.available_in_po) ? 'Yes' : 'No'}</td>
+                                    <td>{(item?.available_in_so) ? 'Yes' : 'No'}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 };
