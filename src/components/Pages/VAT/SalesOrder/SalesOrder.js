b0081c0e62202e45fed3157643614ecef9513c0d
sejan mahmud <sejan157218@gmail.com>
2023-05-21 01:44:25 +0600

    show vat so and po data

--- src/components/Pages/VAT/SalesOrder/SalesOrder.js
+++ src/components/Pages/VAT/SalesOrder/SalesOrder.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Menubar from '../../../Shared/Menubar/Menubar';
+import { Button, Table } from 'react-bootstrap';
+import { UrlContext } from '../../../../App';
 
 const SalesOrder = () => {
+
+    const [salesData, setSalesData] = useState([]);
+    const apiDomain = useContext(UrlContext);
+
+    useEffect(() => {
+        fetch(`${apiDomain}vat/so/`)
+            .then(response => response.json())
+            .then(jsonData => {
+                // console.log(jsonData);
+                setSalesData(jsonData.result);
+            })
+    }, []);
+    console.log("purchaseData", salesData)   
     return (
         <div>
             <Menubar />
-            Sales
+            <div>
+                <h4 className="text-start ms-5">VAT on Sale Order</h4>
+                <Table id="emptyTable" striped bordered hover className="">
+                    <thead>
+                        <tr>
+                            <th>SO ID</th>
+                            <th>Customer</th>
+                            <th>Total Amount</th>
+                            <th>VAT Receivable</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            salesData.map((product, index) => (
+                                <tr key={index}>
+                                    <td>{product.customer}</td>
+                                    <td>{product.so_id}</td>
+                                    <td>{product.so_total}</td>
+                                    <td>{product.vat_total}</td>
+                                    <td>{product.date}</td>
+                                    
+                                </tr>
+                            ))
+                    
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 };
